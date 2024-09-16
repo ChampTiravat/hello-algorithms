b4e555b850800f7fe8321cb1832f409a8d60a3e6
ChampTiravat <tiravat2016@gmail.com>
2024-09-17 06:14:57 +0700

    added c

--- c/main.c
+++ c/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("hello, world!\n");
-    return 0;
+void main() {
+    int numbers[5] = {11, 22, 33, 44, 55};  
+    printf("number of array items: %ld,\n", sizeof(numbers)/sizeof(int));
+
+    for (int i=0; i< 5; ++i) {
+         printf("index: %d: %d,\n", i, numbers[i]);
+    }
 }
